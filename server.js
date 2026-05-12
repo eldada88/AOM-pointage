2e457ae3e0c5a257f05e9e92aa59882510d1e0ca
eldada88 <61236965+eldada88@users.noreply.github.com>
2026-05-12 16:32:11 +0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,68 @@
+const express = require('express');
+[nom]
+);
+
+const user = result.rows[0];
+
+if(!user){
+return res.json({success:false});
+}
+
+const valid = await bcrypt.compare(password,user.password);
+
+if(valid){
+
+res.json({
+success:true,
+user:{
+id:user.id,
+nom:user.nom,
+role:user.role
+}
+});
+
+}else{
+res.json({success:false});
+}
+});
+
+app.post('/logs', async(req,res)=>{
+
+const {
+user_id,
+action,
+timestamp,
+date
+} = req.body;
+
+await pool.query(
+`INSERT INTO logs(
+user_id,
+action_type,
+timestamp,
+work_date
+)
+VALUES($1,$2,$3,$4)`,
+[user_id,action,timestamp,date]
+);
+
+res.json({success:true});
+});
+
+app.get('/logs', async(req,res)=>{
+
+const result = await pool.query(
+`SELECT logs.*, users.nom
+FROM logs
+JOIN users ON users.id = logs.user_id
+ORDER BY timestamp DESC`
+);
+
+res.json(result.rows);
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT,()=>{
+console.log('Serveur démarré');
+});
