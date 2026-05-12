9886036219725501bbe238f19d05f8051746ce03
eldada88 <61236965+eldada88@users.noreply.github.com>
2026-05-12 17:13:05 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,68 +1,100 @@
 const express = require('express');
-[nom]
-);
+const cors = require('cors');
+const bcrypt = require('bcrypt');
+const { Pool } = require('pg');
 
-const user = result.rows[0];
+const app = express();
 
-if(!user){
-return res.json({success:false});
-}
+app.use(cors());
+app.use(express.json());
 
-const valid = await bcrypt.compare(password,user.password);
-
-if(valid){
-
-res.json({
-success:true,
-user:{
-id:user.id,
-nom:user.nom,
-role:user.role
-}
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
-}else{
-res.json({success:false});
-}
+app.get('/', (req, res) => {
+    res.send('API RH Pointage Active');
 });
 
-app.post('/logs', async(req,res)=>{
+app.post('/login', async (req, res) => {
 
-const {
-user_id,
-action,
-timestamp,
-date
-} = req.body;
+    const { nom, password } = req.body;
 
-await pool.query(
-`INSERT INTO logs(
-user_id,
-action_type,
-timestamp,
-work_date
-)
-VALUES($1,$2,$3,$4)`,
-[user_id,action,timestamp,date]
-);
+    const result = await pool.query(
+        'SELECT * FROM users WHERE nom=$1',
+        [nom]
+    );
 
-res.json({success:true});
+    const user = result.rows[0];
+
+    if (!user) {
+        return res.json({ success: false });
+    }
+
+    const valid = await bcrypt.compare(password, user.password);
+
+    if (valid) {
+
+        res.json({
+            success: true,
+            user: {
+                id: user.id,
+                nom: user.nom,
+                role: user.role
+            }
+        });
+
+    } else {
+
+        res.json({
+            success: false
+        });
+
+    }
 });
 
-app.get('/logs', async(req,res)=>{
+app.post('/logs', async (req, res) => {
 
-const result = await pool.query(
-`SELECT logs.*, users.nom
-FROM logs
-JOIN users ON users.id = logs.user_id
-ORDER BY timestamp DESC`
-);
+    const {
+        user_id,
+        action,
+        timestamp,
+        date
+    } = req.body;
 
-res.json(result.rows);
+    await pool.query(
+        `INSERT INTO logs(
+            user_id,
+            action_type,
+            timestamp,
+            work_date
+        )
+        VALUES($1,$2,$3,$4)`,
+        [user_id, action, timestamp, date]
+    );
+
+    res.json({
+        success: true
+    });
+});
+
+app.get('/logs', async (req, res) => {
+
+    const result = await pool.query(
+        `SELECT logs.*, users.nom
+         FROM logs
+         JOIN users ON users.id = logs.user_id
+         ORDER BY timestamp DESC`
+    );
+
+    res.json(result.rows);
 });
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT,()=>{
-console.log('Serveur démarré');
+app.listen(PORT, () => {
+    console.log('Serveur démarré');
 });
